e0d435d74bfbf554e382b943af1260379b9e5812
dhiaabdelli <dhia.abdelli1@esprit.tn>
2020-12-23 23:13:32 +0000

    update

--- INTEGRATION/login.cpp
+++ INTEGRATION/login.cpp
@@ -12,6 +12,8 @@ bool login::sign_in(QString uname,QString pwd)
     qry.bindValue(":username",uname);
     qry.bindValue(":password",this->hash(pwd));
 
+    this->current_user=uname;
+
     return qry.exec() && qry.next();
 
 }
@@ -27,8 +29,21 @@ bool login::sign_up(QString uname,QString pwd)
     return qry.exec();
 }
 
+bool login::modifier_mdp(QString user,QString old_mdp,QString new_mdp)
+{
+    QSqlQuery qry;
+    qry.prepare("UPDATE USERS SET password=:new_password WHERE (username=:user AND password=:old_password)");
+    qry.bindValue(":user",user);
+    qry.bindValue(":old_password",this->hash(old_mdp));
+    qry.bindValue(":new_password",this->hash(new_mdp));
+
+    return qry.exec();
+}
+
 int login::hash(QString pwd)
 {
     std::hash<std::string> hash;
     return hash(pwd.toStdString());
 }
+
+
