06ebc68e256d5d9233b85e11ad2dbc9eda0d0a7a
dhiaabdelli <dhia.abdelli1@esprit.tn>
2020-12-24 00:36:47 +0000

    update

--- INTEGRATION/connexion.cpp
+++ INTEGRATION/connexion.cpp
@@ -1,38 +1,27 @@
 #include "connexion.h"
-
-
+#include "QSqlDatabase"
 connexion::connexion()
 {
 
 }
 
-bool connexion::createConnection()
-{
-    bool test=false;
-    QSqlDatabase db=QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("gestion_invites");
-    db.setUserName("dhia");
-    db.setPassword("dhia");
+bool connexion::createconnect()
+{bool test=false;
+QSqlDatabase db = QSqlDatabase::addDatabase("QODBC");
+db.setDatabaseName("testqt");
+db.setUserName("ELLA");//inserer nom de l'utilisateur
+db.setPassword("ella");//inserer mot de passe de cet utilisateur
 
-    if (db.open())
-        test=true;
+if (db.open())
+test=true;
 
-    return test;
+
+
+
+
+    return  test;
 }
 
-bool connexion::createConnexion()
-{
-    bool test=false;
-    QSqlDatabase db=QSqlDatabase::addDatabase("QODBC");
-    db.setDatabaseName("gestion_invites");
-    db.setUserName("dhia");
-    db.setPassword("dhia");
-
-    if (db.open())
-        test=true;
-
-    return test;
-}
 
 
 
